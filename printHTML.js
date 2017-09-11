227dddf5bbf90172752de81e59f0be647271198c
evgenyg6 <evgenyg@live.com>
2017-09-11 16:31:46 +0000

    Another needless commit after modfying al ine of code

--- printHTML.js
+++ printHTML.js
@@ -1,11 +1,29 @@
 function getAndPrintHTML () {
   var https = require('https');
+
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step2.html'
+    path: '/http-examples/step1.html'
   };
 
-  /* Add your code here */
+  function callback(response){
+    var empty;
+
+    response.on('data', function (data) {
+      empty += data.toString();
+      console.log('Chunk Received. Length:', data.length);
+    });
+    response.on('end', function(){
+      console.log("Response stream conplete.")
+      console.log(empty);
+    });
+
+  }
+
+
+
+
+  https.request(requestOptions, callback).end();
 
 }
 getAndPrintHTML();
